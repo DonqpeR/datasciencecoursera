20f94459e589fb2be3fb1f78f371368aadcb2489
DonqpeR <jose.pinter@gmail.com>
2015-04-26 23:41:26 +0200

    Text was missing from Helloword.R

--- R Programming/complete.R
+++
@@ -1,8 +0,0 @@
-complete <- function(directory, id = 1:332) {
-  result <- data.frame(id = numeric(), nobs = numeric())
-     for ( i in id ) { 
-         df <- read.csv(paste0(directory,"/",formatC(i, width=3, flag="0"),".csv"))
-         result <- rbind(result, data.frame(id = i, nobs = sum(complete.cases(df), deparse.level=0)))
-         }
-  result
-}
