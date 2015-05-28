e0fd6fd8317726bf68ae4e03a30da3d338bc4bc4
DonqpeR <jose.pinter@gmail.com>
2015-05-28 22:31:03 +0200

    Fixed activity name replacement. Simplified code and removed some unnecessary calls.

--- run_analysis.R
+++ run_analysis.R
@@ -12,8 +12,8 @@ if (!file.exists(filename))
 folder <- "UCI HAR Dataset"
 
 # Loading helper objects for determining meaningful activity names and selecting required measurements
-activities <- tbl_df( read.table( file = paste( folder, "activity_labels.txt", sep="/" ) ) )
-features <- tbl_df( read.table( file = paste( folder, "features.txt", sep="/" ) ) )
+activities <- read.table( file = paste( folder, "activity_labels.txt", sep="/" ))
+features <- read.table( file = paste( folder, "features.txt", sep="/" ))
 
 # Preparing vectors for loading only the required columns (mean and sd of each measurement)
 # instead of loading all and selecting the columns later
@@ -31,14 +31,16 @@ x_test <- read.table( file = paste( folder, "test/x_test.txt", sep="/" )
 y_test <- read.table( file = paste( folder, "test/y_test.txt", sep="/" )
                     , col.names = "activity"
                     )
-mutate( y_test, activity <- activities[y_test$activity, 2] )
+y_test$activity <- activities[y_test$activity, 2]
 
 subject_test <- read.table( file = paste( folder, "test/subject_test.txt", sep="/" )
                           , col.names = "subject_id"  
                           )
 
-merged_test <- merge( subject_test, y_test, by="row.names", sort=FALSE ); merged_test$Row.names <- NULL
-merged_test <- merge( merged_test, x_test, by="row.names", sort=FALSE ); merged_test$Row.names <- NULL 
+#merged_test <- merge( subject_test, y_test, by="row.names", sort=FALSE ); merged_test$Row.names <- NULL
+#merged_test <- merge( merged_test, x_test, by="row.names", sort=FALSE ); merged_test$Row.names <- NULL 
+merged_test <- cbind( x_test, cbind( subject_test, y_test )) 
+
 
 ### Loading and merging train data 
 x_train <- read.table( file = paste( folder, "train/x_train.txt", sep="/" )
@@ -49,18 +51,19 @@ x_train <- read.table( file = paste( folder, "train/x_train.txt", sep="/" )
 y_train <- read.table( file = paste( folder, "train/y_train.txt", sep="/" )
                      , col.names = "activity"
                      )
-mutate( y_train, activity <- activities[y_train$activity, 2] )
+y_train$activity <- activities[y_train$activity, 2]
 
 subject_train <- read.table( file = paste( folder, "train/subject_train.txt", sep="/" )
                            , col.names = "subject_id"  
                            )
 
-merged_train <- merge( subject_train, y_train, by="row.names", sort=FALSE ); merged_train$Row.names <- NULL
-merged_train <- merge( merged_train, x_train, by="row.names", sort=FALSE ); merged_train$Row.names <- NULL 
+#merged_train <- merge( subject_train, y_train, by="row.names", sort=FALSE ); merged_train$Row.names <- NULL
+#merged_train <- merge( merged_train, x_train, by="row.names", sort=FALSE ); merged_train$Row.names <- NULL 
+merged_train <- cbind( x_train, cbind( subject_train, y_train )) 
 
 # Combining test and train data 
 merged_all <- rbind(merged_test, merged_train)
 
-### Creating data set with average values of per activities and subjects
+### Creating data set with average values per activities and subjects
 averages <- merged_all %>% group_by(activity, subject_id) %>% summarise_each("mean")
 write.table( x = averages, file = "averages.txt", row.names = FALSE )
